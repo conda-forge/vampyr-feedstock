6aa7b4bfd9c6ccdbabf3071d9506ee738c37f286
Roberto Di Remigio Eikås <robertodr@users.noreply.github.com>
2023-12-08 08:40:29 +0100

    Minor change in build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,8 +4,15 @@ CXXFLAGS="${CXXFLAGS//-mtune=haswell}"
 # needed for macOS see here: https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
 CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 
+ARCH_ARGS=""
+IS_PYPY=$(${PYTHON} -c "import platform; print(int(platform.python_implementation() == 'PyPy'))")
+
+if [[ $IS_PYPY == 1 ]]; then
+    ARCH_ARGS="-DPython_INCLUDE_DIR=${PREFIX}/include/pypy${PY_VER} ${ARCH_ARGS}"
+fi
+
 # configure
-cmake ${CMAKE_ARGS} \
+cmake ${CMAKE_ARGS} ${ARCH_ARGS} \
   -S${SRC_DIR} \
   -Bbuild \
   -GNinja \
@@ -13,7 +20,7 @@ cmake ${CMAKE_ARGS} \
   -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
   -DENABLE_ARCH_FLAGS=OFF \
   -DCMAKE_CXX_COMPILER=${CXX} \
-  -DPYMOD_INSTALL_LIBDIR="${SP_DIR#$PREFIX/lib}"
+  -DPYMOD_INSTALL_FULLDIR="${SP_DIR#$PREFIX/lib}"
 
 
 # build and install (testing is done later)
